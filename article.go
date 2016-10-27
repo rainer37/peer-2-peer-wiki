ca53ddfa4d6b00027216ace94070cb3076180c0a
nickbradley <nickbradley@live.ca>
2016-10-27 10:35:46 -0700

    Added/Updated methods for article

--- article.go
+++ article.go
@@ -2,6 +2,7 @@ package p2pwiki
 
 import (
   "time"
+  "itc"
 )
 
 path string = "/Path/to/file"
@@ -10,7 +11,7 @@ type Article struct {
   Title string,
   Content string,
 
-  version int,
+  version *itc.Stamp,
   timestamp int,  // unix timestamp
   size int  // bytes
 }
@@ -19,29 +20,61 @@ func NewArticle(title string, content string) *Article {
   a := new(Article)
   a.Title = title
   a.Content = content
-  a.version = 1
+  a.version = itc.NewStamp()
   a.timestamp = time.Now().Unix()
   a.size = len(title) + len(content)
   return a
 }
 // Read article from disk
-func ReadArticle(title string, version int) *Article {
-}
-// Modify article
-func (a *Article) Update(content string) *Article {
-  b := NewArticle(a.Title, content)
-  b.version++
-  return b
-}
-// Returns all versions numbers of the article
-func (a *Article) Versions() []int {
-
+func ReadArticle(title string) *Article {
 }
 // Write article to disk
-func (a *Article) Flush() (err Error) {
+func (a *Article) Flush() error {
 
 }
 // Delete the article from disk
-func (a *Article) Remove() (err Error) {
+func (a *Article) Remove() error {
 
+}
+
+
+// Modify article
+func (a *Article) Modify(content string) {
+  a.Content = content
+  a.version.Event()
+}
+
+// Compare article Versions
+func (a *Article) IsNewer(b *Article) (bool, error) {
+  if a.Title != b.Title {
+    return false, new Error("Cannot compare articles with different titles.")
+  }
+  return a.version.LEQ(b.version), nil
+}
+
+// Duplicate article and give it a new version id
+func (a *Article) Replicate() *Article {
+  b := a
+  b.version = a.version.Fork()
+  return b
+}
+
+// Combine an article with a replica
+func (a *Article) Merge(b *Article) error {
+  if a.Title != b.Title {
+    return new Error("Can't merge articles with different titles.")
+  }
+  a.version = a.version.Join(b.version)
+
+  return nil
+}
+
+// Convert the article to a byte[] for transmission
+func (a *Article) Marshal() ([]byte, error) {
+
+}
+
+// Convert byte[] to article
+func Unmarshal(data []byte) (*Article, error) {
+  
 }
--- peer.go
+++ peer.go
@@ -18,5 +18,9 @@ func (this *Peer) locate(n int) []string {
 // Replicate an article to a peer.
 // Calls the remote peer's Article.Update method using RPC.
 func replicate(article *Article, address string) (err Error) {
+  artRep := article.replicate()
 
+  msg := artReg.Marshal()
+
+  // send msg to address
 }
