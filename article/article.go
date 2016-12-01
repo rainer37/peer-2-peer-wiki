db034df6efcd4137e0530f0b0948d0c466d303a7
nickbradley <nickbradley@live.ca>
2016-12-01 14:08:47 -0800

    Added Push and Pull methods

--- article/article.go
+++ article/article.go
@@ -4,16 +4,14 @@ import (
   "fmt"
 )
 
-// TODO Deal with Vector Clock.
 // TODO Need some way to push/pull articles.
 
 
 
 type Article struct {
   Title string
-  Hist Treedoc
-  //Version VectorClock
-  Buffer []OpLog
+  Hist Treedoc  // The Treedoc CRDT
+  Log []OpLog  // store the local insert/delete operations to be replayed on the host
 }
 
 
@@ -32,6 +30,40 @@ func OpenArticle(path string, title string) (*Article, error) {
 
   return &a, err
 }
+
+
+// NOTE @Rain This will be an RPC
+// updates the artivle param to point to the article with the soecified title
+func PullArticle(title string, article *Article) error {
+  // This executes on the server using the title requested by the client
+  a,err := OpenArticle("../articles/", title)
+  if err != nil {
+    return err
+  }
+
+  article = a
+
+  return nil
+}
+
+
+// NOTE @Rain This will be an RPC
+// NOTE Don't think we need replayCount but RPC methods require two params
+// NOTE We don't need to send the entire article -- just the title and the log.
+//      It's easier to just send the entire article though
+// This is supposed to execute on the server. The client C sends it's copy of the
+// article to the server S. S replays C's log on the (shared) version of the article.
+func Push(remoteArticle Article, replayCount *int) error {
+  // This should be exectued on the server
+  a,err := OpenArticle("../articles/", article.Title)
+  if err != nil {
+    return err
+  }
+
+  // The replay of the client's log is executed on the server
+  return a.Replay(remoteArticle)
+}
+
 
 // Write the article to the specified path as a JSON file. The file will be named
 // with the title of the article.
@@ -63,7 +95,7 @@ func (a *Article) Insert(pos int, atom Atom, site Disambiguator) error {
   }
 
   // insert into log
-  a.Buffer = append(a.Buffer, Operation{"insert", n})
+  a.Buffer = append(a.Log, Operation{"insert", n})
 }
 
 func (a *Article) Delete(pos int, site Disambiguator) error {
@@ -72,13 +104,13 @@ func (a *Article) Delete(pos int, site Disambiguator) error {
     return err
   }
 
-  a.Buffer = append(a.Buffer, Operation{"delete", n})
+  a.Buffer = append(a.Log, Operation{"delete", n})
 }
 
 // Replay all commands in the buffer
-func (a *Article) FlushBuffer() error {
+func (a *Article) Replay(remoteLog OpLog) error {
   var err error
-  for _,op := range a.Buffer {
+  for _,op := range remoteLog {
     switch op {
     case "insert":
       err = a.Hist.insertNode(op.Node)
